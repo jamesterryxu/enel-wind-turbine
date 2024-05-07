c6eb8f768e4acd1be315877fc7b96771504ef271
jamesterryxu <james.xu@berkeley.edu>
2024-05-07 19:04:26 +0100

    adding changes to plotting functions

--- python/utils/plotting.py
+++ python/utils/plotting.py
@@ -17,7 +17,17 @@ import matplotlib.ticker as ticker
 from matplotlib.lines import Line2D
 
 # import some utitlity functions
-from utils.utils import load_min_max_luna_data
+from utils.utils import load_min_max_luna_data, load_preprocessed_das_data
+
+# for debugging
+# import pdb; pdb.set_trace()
+
+## some general plotting conventions to be used here:
+## - when plotting circumferential data, plot looking down, with cardinal north being '90' degrees
+## - if possible, plot the wind direction and speed as well
+## - when dealing with luna data, make sure to know how many nan's are being plotted
+## - when plotting circumferential data, be careful on the direction of plotting (ccw or cw), will plot based on the 'x' data
+
 
 ### luna plotting functions
 def plot_min_max_luna(directory_to_file, input_file_name,title,transparency=0.25,start_point = 0,strain_min = -20, strain_max=20):
@@ -90,12 +100,168 @@ def plot_min_max_luna(directory_to_file, input_file_name,title,transparency=0.25
 def plot_min_max_luna_comparison(directory_to_file, list_of_input_file_name):
     ''' Function to plot several files of processed min, max data
     Args:
-        directory_to_file:
+        directory_to_file: directory to file location
         list_of_input_file_name: list of file names to plot
 
     Returns:
     '''
     return
+
+
+
+### das plotting functions
+def plot_das_time_series(directory_to_file, input_file_name,channels,title,transparency=0.25,start_point = 0,strain_min = -20, strain_max=20):
+    ''' Functino to plot time series data of das data
+    Args:
+        directory_to_file: directory to file location
+        list_of_input_file_name: list of file names to plot
+        channels: list of channel indicies
+
+    Returns:
+    '''
+    # list of tower segments
+    # segments = ['bot_a', 'mid_a', 'top_a',
+    #             'bot_b', 'mid_b', 'top_b',
+    #             'bot_c', 'mid_c', 'top_c',
+    #             'bot_d', 'mid_d', 'top_d']
+    segments = ['bot_a', 
+                'bot_b', 
+                'bot_c',
+                'bot_d']
+    
+    strain,time_datetime,_ = load_preprocessed_das_data(directory_to_file = directory_to_file,
+                               input_file_name = input_file_name)
+    
+    # plot segments
+    fig,axs = plt.subplots(2,2,figsize=(30,10),dpi=400)
+
+    for segment in segments:
+        if '_a' in segment:
+            print(np.size(time_datetime),np.shape(strain[segment]))
+            axs[0,0].scatter(time_datetime,strain[segment][:,0],
+                             color='tab:blue',s=10,zorder=1,alpha=transparency)
+        elif '_b' in segment:
+            axs[0,1].scatter(time_datetime,strain[segment][:,0],
+                             color='tab:blue',s=10,zorder=1,alpha=transparency)
+        elif '_c' in segment:
+            axs[1,0].scatter(time_datetime,strain[segment][:,0],
+                             color='tab:blue',s=10,zorder=1,alpha=transparency)
+        elif '_d' in segment:
+             axs[1,1].scatter(time_datetime,strain[segment][:,0],
+                             color='tab:blue',s=10,zorder=1,alpha=transparency)
+             
+    
+    # set up titles and formatting
+    axs[0,0].set_title('Axis a', fontsize=24)
+    axs[0,1].set_title('Axis b', fontsize=24)
+    axs[1,0].set_title('Axis c', fontsize=24)
+    axs[1,1].set_title('Axis d', fontsize=24)
+    axs[0,0].set_ylabel('micro Strain Envelope',fontsize=16)
+    axs[1,0].set_ylabel('micro Strain Envelope',fontsize=16)
+    axs[1,0].set_xlabel('Time',fontsize=16)
+    axs[1,1].set_xlabel('Time',fontsize=16)
+    axs[0,0].tick_params(axis='both', labelsize=16)
+    axs[1,0].tick_params(axis='both', labelsize=16)
+    axs[1,0].grid(True)
+    axs[0,0].grid(True)
+    axs[1,1].grid(True)
+    axs[0,1].grid(True)
+    fig.suptitle(title, fontsize=28)
+    plt.tight_layout(rect=[0, 0, 1, 0.99])
+    plt.tight_layout()
+    plt.show()
+    return
+
+
+
+
+def plot_das_time_series_one_axis(directory_to_file, input_file_name, axis='a', title='DAS Time Series', transparency=0.25):
+    ''' 
+    Function to plot time series data of DAS data for all indices within a specified axis in individual subplots, stacked vertically.
+    
+    Args:
+        directory_to_file: directory to file location
+        input_file_name: file name to plot
+        axis: specific axis to plot ('a', 'b', 'c', or 'd')
+        title: title of the plot
+
+    '''
+    # Load data
+    strain,time_datetime,_ = load_preprocessed_das_data(directory_to_file = directory_to_file,
+                               input_file_name = input_file_name)
+    
+    bot_name = 'bot_' + axis
+    mid_name = 'mid_' + axis
+    top_name = 'top_' + axis
+    
+    # Calculate total height of each segment assuming 1 m spacing
+    segment_height = strain[bot_name].shape[0]
+    mid_offset = segment_height + 2 # 2 meter gap between bottom and mid
+    top_offset = mid_offset + segment_height + 2 # 2 meter gap between mid and top
+
+    # Create distance array
+    distance_bot = np.arange(segment_height)
+    distance_mid = np.arange(segment_height) + mid_offset
+    distance_top = np.arange(segment_height) + top_offset
+    total_distance = np.concatenate((distance_bot, distance_mid, distance_top))
+    # import pdb; pdb.set_trace()
+    # Concatenating strain data
+    total_strain = np.vstack([strain[bot_name], strain[mid_name], strain[top_name]])
+
+    ## Plotting
+    # plotting strain for each sensor
+    fig, ax1 = plt.subplots(1, 1, figsize=(12, 6))
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Microstrain')
+    for t in range(total_strain.shape[1]):
+        ax1.plot(time_datetime,total_strain[:, t], color='tab:blue', alpha=transparency)
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+
+    # plotting distance for each sensor
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Distance (m)', color='tab:red')
+    ax2.plot(time_datetime, total_distance, color='tab:red', alpha=transparency)
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+
+    fig.tight_layout()
+    plt.show()
+    
+
+    # # Number of sensors (indices)
+    # num_sensors = strain['bot_a'].shape[1]
+
+    # # Find global min and max strain values for uniform y-axis scaling
+    # min_strain = np.min(strain[f'bot_{axis}'])
+    # max_strain = np.max(strain[f'bot_{axis}'])
+
+    # # Setup plot - determine layout
+    # fig, axs = plt.subplots(num_sensors, 1, figsize=(12, 2 * num_sensors), sharex=True)
+    # fig.suptitle(title, fontsize=16)
+
+    # # Plot each sensor's time series in its own subplot
+    # for i in range(num_sensors):
+    #     if num_sensors == 1:
+    #         ax = axs
+    #     else:
+    #         ax = axs[i]
+    #     ax.plot(time_datetime, strain['bot_a'][:, i], color='tab:blue', alpha=transparency)
+    #     ax.grid(True)
+    #     ax.set_ylim(min_strain, max_strain) # Uniform y-axis scaling
+    #     # Remove horizontal spines
+    #     ax.spines['top'].set_visible(False)
+    #     ax.spines['bottom'].set_visible(False)
+    #     # Add right y-axis with sensor number
+    #     sec_ax = ax.secondary_yaxis('right')
+    #     sec_ax.set_yticks([])
+    #     sec_ax.set_ylabel(f'Sensor {i + 1}')
+    # Labeling and layout adjustments
+    # axs[-1].set_xlabel('Time')
+    # axs[0].set_ylabel('Microstrain')
+    # plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust the layout to make room for the title
+    # plt.show()
+
+
+
 
 
 # class strain_animation():
